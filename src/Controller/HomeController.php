a2489f9701dfa1e88741e87d59aca9f4227a0485
PapaSARR <dadscientific@hotmail.com>
2021-03-24 13:09:41 +0100

    Réalisation de la page de contact avec formBuilder

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Post;
+use App\Form\Type\ContactType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,11 +42,25 @@ class HomeController extends AbstractController
     /**
      * @Route("/posts}", name="posts", methods={"GET"})
      */
-    public function all_posts(): Response
+    public function posts(): Response
     {
 
         return $this->render('home/posts.html.twig', [
             'posts' => $this->postRepo->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/contact}", name="contact", methods={"GET", "POST"})
+     */
+    public function contact(): Response
+    {
+        // creates a task object and initializes some data for this example
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        return $this->render('home/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
 }
