3f4a33204894d536cab298723467761d64a0af6b
PapaSARR <dadscientific@hotmail.com>
2021-03-31 15:15:58 +0200

    CRUDs pour les entités User, Contact & Newsletter

--- src/Controller/Admin/BlogAdminController.php
+++ src/Controller/Admin/BlogAdminController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use App\Entity\Contact;
+use App\Entity\Newsletter;
 use App\Entity\Post;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -35,7 +38,10 @@ class BlogAdminController extends AbstractDashboardController
         //yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         //yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         yield MenuItem::linktoRoute('Retour à l\'accueil', 'fas fa-home', 'home');
+        yield MenuItem::linkToCrud('User', 'fas fa-smile', User::class);
         yield MenuItem::linkToCrud('Post', 'fas fa-edit', Post::class);
         yield MenuItem::linkToCrud('Commentaire', 'fas fa-comments', Comment::class);
+        yield MenuItem::linkToCrud('Contact', 'fas fa-at', Contact::class);
+        yield MenuItem::linkToCrud('Newsletter', 'fas fa-envelope', Newsletter::class);
     }
 }
